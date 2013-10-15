597ca110d275c7ce3f9a1fb9ffab41cfa56f32e0
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2013-10-15 17:21:10 +0900

    add smpt settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,15 @@ LoPhoto::Application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
 end
