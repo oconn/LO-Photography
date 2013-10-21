6dcf72ea13b5b09aea3bae762d56b653a00663ad
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2013-10-20 15:19:36 -1000

    s3 https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,8 @@ LoPhoto::Application.configure do
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_protocol => https
     }
   }
 
