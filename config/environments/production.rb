0070c02cdad7b89e1622e48076c6b314bef6314d
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2013-10-20 16:00:03 -1000

    s3 secure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ LoPhoto::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_protocol => https,
+    :s3_protocol => "https",
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
