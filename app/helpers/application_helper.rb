408720c8acdbf205cae1269cde1ff9acb31fff6b
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2013-10-20 14:40:34 -1000

    update for firefly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "L.O. Photography"
+    base_title = "Firefly Photography"
     if page_title.empty?
       base_title
     else
