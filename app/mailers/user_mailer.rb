f737a28aeea1b76e4296df1ee55a0a4858000b0c
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2013-10-30 20:52:28 -1000

    add email from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,8 @@ class UserMailer < ActionMailer::Base
     @inquiry = inquiry
     mail(:subject => "Firefly Contact",
     	   :to      => "leah@lofirefly.com",
-    	   :bcc     => "matthew.j.oconnell1@gmail.com")
+    	   :bcc     => "matthew.j.oconnell1@gmail.com",
+    	   :from    => "@inquiry.email")
   end
  
 end
