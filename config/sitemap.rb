6113763eb1cae9cf62c2e78a95a103fa2e066f1c
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2013-11-07 06:15:58 -1000

    dynamic update sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -42,4 +42,8 @@ SitemapGenerator::Sitemap.create do
     add post_path(post), :lastmod => post.updated_at
   end
 
+  after "deploy", "refresh_sitemaps"
+  task :refresh_sitemaps do
+    run "cd #{latest_release} && RAILS_ENV=#{rails_env} rake sitemap:refresh"
+  end
 end
