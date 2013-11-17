4ba747721cd2cca737e518da5086335e0ae95736
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2013-11-17 08:20:35 -1000

    config aws bucket

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -2,7 +2,7 @@ class SitemapsController < ApplicationController
 
   def show
     # Redirect to CloudFront and S3
-    redirect_to "http://your-distribution-id.cloudfront.net/sitemaps/sitemap1.xml.gz"
+    redirect_to "http://s3.amazonaws.com/lo-photography/sitemaps/sitemap1.xml.gz"
   end
 
 end
